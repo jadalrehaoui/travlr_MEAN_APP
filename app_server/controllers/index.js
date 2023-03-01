bfb6344c84ab9db4744eed924cfa751da8af5f57
jadalrehaoui <jrehaoui.dev@outlook.com>
2023-02-28 18:11:35 -0600

    Initial commit

---
+++ app_server/controllers/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    main: require("./main"),
+    travel: require("./travel"),
+    meals: require("./meals"),
+    news: require("./news"),
+    rooms: require("./rooms"),
+    about: require("./about"),
+    contact: require("./contact"),
+}
---
+++ app_server/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var controllers = require('../controllers');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', controllers.main);
+
+router.get('/travel', controllers.travel)
+
+router.get('/rooms', controllers.rooms)
+
+router.get('/meals', controllers.meals)
+
+router.get('/news', controllers.news)
+
+router.get('/about', controllers.about)
+
+router.get('/contact', controllers.contact)
+
+module.exports = router;
